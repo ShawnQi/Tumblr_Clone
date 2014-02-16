dbe14aecee4fcda9ce59d01082700cd172f9cb66
Raman Sycheuski <raman.sycheuski@gmail.com>
2014-02-16 14:23:50 -0500

    Adds posts controller. Creates posts and drafts actions and basic views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 TumblrClone::Application.routes.draw do
   root to: 'users#show'
   
-  resources :users
-  
   get 'login', to: 'sessions#new', as: 'login'
   post 'signin', to: 'sessions#create', as: 'signin'
   delete 'signout', to: 'sessions#destroy', as: 'signout'
+  
+  resources :users
+  resources :posts
+  get 'drafts', to: 'posts#drafts', as: 'drafts'
 end
