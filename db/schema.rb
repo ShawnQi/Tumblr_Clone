6e9c4794e2add8d5953debf0ea26a6c0d98ab191
Raman Sycheuski <raman.sycheuski@gmail.com>
2014-02-16 14:05:13 -0500

    Adds posts table. Creates associations between users and posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140216034612) do
+ActiveRecord::Schema.define(:version => 20140216183425) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "draft",      :default => false
+    t.integer  "user_id"
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+  end
+
+  add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "username"
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,20 @@ User.create!({email: "user@demo.com",
               password: "password",
               username: "demo_user",
               blog_name: "demo_blog"})
+              
+Post.create!({title: "Ninon de L'Enclos",
+              body: "The joy of the mind is the measure of its strength.",
+              draft: false,
+              user_id: 1})
+Post.create!({title: "Tom Lehrer",
+              body: "Bad weather always looks worse through a window.",
+              draft: false,
+              user_id: 1})
+Post.create!({title: "Ninon de L'Enclos",
+              body: "Love of beauty is taste. The creation of beauty is art.",
+              draft: false,
+              user_id: 1})
+Post.create!({title: "Ralph Waldo Emerson",
+              body: "The joy of the mind is the measure of its strength.",
+              draft: true,
+              user_id: 1})
