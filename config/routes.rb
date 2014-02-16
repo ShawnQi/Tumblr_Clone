36088212a056682c8ab38d178efc6318a0b904d9
Raman Sycheuski <raman.sycheuski@gmail.com>
2014-02-16 15:11:56 -0500

    Changes routes for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ TumblrClone::Application.routes.draw do
   post 'signin', to: 'sessions#create', as: 'signin'
   delete 'signout', to: 'sessions#destroy', as: 'signout'
   
-  resources :users
+  resources :users, only: [:new, :create, :show]
   
-  resources :posts
+  resources :posts, only: [:index, :edit, :update, :destroy]
   get 'drafts', to: 'posts#drafts', as: 'drafts'
   get 'posts/:id/publish', to: 'posts#publish', as: 'publish'
 end
