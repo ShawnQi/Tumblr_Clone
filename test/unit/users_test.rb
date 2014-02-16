3eab031e56c16d89547e13b5183a125e97391062
Raman Sycheuski <raman.sycheuski@gmail.com>
2014-02-15 23:29:42 -0500

    Adds users model. Sets up validations, password_digest and session_token

---
+++ test/unit/users_test.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string(255)
+#  email           :string(255)
+#  blog_name       :string(255)
+#  password_digest :string(255)
+#  session_token   :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+require 'test_helper'
+
+class UsersTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
