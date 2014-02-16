41267e692bd7dd5a9f558bd55487c85a68fff6a8
Raman Sycheuski <raman.sycheuski@gmail.com>
2014-02-16 14:50:42 -0500

    Adds publish action. Adds notifications on top for application layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,17 @@ class PostsController < ApplicationController
   def drafts
     @drafts = current_user.posts.where(draft: true)
   end
+  
+  def publish
+    @draft = current_user.posts.where(draft: true).find(params[:id])
+    
+    if @draft
+      @draft.update_attributes({draft: false})
+      flash[:main] = "Your draft has been published"
+      redirect_to posts_url
+    else
+      flash[:main] = "There was an error in trying to publish your draft"
+      redirect_to drafts_url
+    end
+  end
 end
