02d96d0142fb9f0897408fb29a6e9ab44365b9b0
Raman Sycheuski <raman.sycheuski@gmail.com>
2014-02-17 09:50:56 -0500

    Adds likes model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140217143724) do
+ActiveRecord::Schema.define(:version => 20140217144919) do
 
   create_table "activs", :force => true do |t|
     t.string   "sent_title"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(:version => 20140217143724) do
 
   add_index "followings", ["followed_id"], :name => "index_followings_on_followed_id"
   add_index "followings", ["follower_id"], :name => "index_followings_on_follower_id"
+
+  create_table "likes", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "likes", ["post_id"], :name => "index_likes_on_post_id"
+  add_index "likes", ["user_id"], :name => "index_likes_on_user_id"
 
   create_table "posts", :force => true do |t|
     t.string   "title"
