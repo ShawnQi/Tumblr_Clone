f224116b711cf5ebfa00b11d10625c952b1240f9
Raman Sycheuski <raman.sycheuski@gmail.com>
2014-02-17 16:00:09 -0500

    Fixes n+1 issue with the dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def current_user
     return nil unless session[:session_token]
-    current_user ||= User.find_by_session_token(session[:session_token])
+    @current_user ||= User.find_by_session_token(session[:session_token])
   end
   
   def signin(user)
