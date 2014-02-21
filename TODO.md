e807839b6aa73b0696c5a5cab212a0079728bae5
Raman Sycheuski <raman.sycheuski@gmail.com>
2014-02-21 12:04:17 -0500

    Adds styling to flash messages

--- TODO.md
+++ TODO.md
@@ -1,10 +1,10 @@
 * Pluralize following and followers pages
----
 * Add clickable blog link everywhere
 * Add quotes to blogs name in findblogs and grey out username
 * Change activity menu to number
 * Change demo user's info/attributes on login
 * Add home pointer everywhere
+---
 * flash messages
 * Add avatars
 * Models rspec
--- app/controllers/activs_controller.rb
+++ app/controllers/activs_controller.rb
@@ -3,7 +3,7 @@ class ActivsController < ApplicationController
   before_filter :get_menu_stats
   
   def index
-    @activs = Activ.where('sent_user_id=? OR got_user_id=?', current_user.id, current_user.id)
+    @activs = Activ.where('got_user_id=?', current_user.id)
                    .order("created_at DESC")
                    .limit(20)
   end
