73f60122c6b147e736b0e105cc5c3e20204dcacb
Raman Sycheuski <raman.sycheuski@gmail.com>
2014-02-25 16:27:22 -0500

    Adds redirection after avatar upload

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,7 +45,8 @@ module ApplicationHelper
     User.find(1).update_attributes({email: "user@example.com",
                                     password: "password",
                                     username: "demo_user",
-                                    blog_name: "My Demo Blog"})
+                                    blog_name: "My Demo Blog",
+                                    avatar: nil})
   
     Post.create!({title: "Michael Jordan",
                   body: "I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.",
