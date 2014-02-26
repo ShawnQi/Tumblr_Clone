48970d2ebcd1aad329a10cde73d9f5552b1b57bf
Raman Sycheuski <raman.sycheuski@gmail.com>
2014-02-26 11:21:07 -0500

    Completes mailer for forgetting a password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,13 +28,30 @@ class SessionsController < ApplicationController
     redirect_to login_url
   end
   
-  def password_reset_view
+  def password_reset_new
   end
   
-  def password_reset
+  def password_reset_send
     user = User.find_by_email(params[:user][:email])
     user.send_password_reset if user
     flash[:main] = "Email sent with password reset instructions"
     redirect_to root_url
+  end
+  
+  def password_reset_edit
+    @user = User.find_by_password_reset_token!(params[:id])
+  end
+  
+  def password_reset_update
+    @user = User.find_by_password_reset_token!(params[:id])
+    if @user.password_reset_sent_at < 2.hours.ago
+      flash[:main] = "Password reset has expired"
+      redirect_to password_reset_new_url
+    elsif @user.update_attributes(params[:user])
+      flash[:main] = "Password reset has reset"
+      redirect_to login_url
+    else
+      render :password_reset_edit
+    end
   end
 end
