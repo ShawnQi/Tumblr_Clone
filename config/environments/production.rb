d24bcc9ba905c1d6695a1d8a9f6b0dae6e7c3a30
Raman Sycheuski <raman.sycheuski@gmail.com>
2014-02-26 12:07:56 -0500

    minor fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,14 +77,14 @@ TumblrClone::Application.configure do
   }
   
   config.action_mailer.default_url_options = { host: "www.tumblr-clone.com" }
-  
+  # ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
-  ActionMailer::Base.delivery_method ||= :smtp
 end
